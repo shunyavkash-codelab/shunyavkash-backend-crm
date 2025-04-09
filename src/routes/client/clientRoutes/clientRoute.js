515b289950813d2627bfe0e16df7116f4ae396a6
shunyavkash-satish-kanaujiya <satish.kanaujiya@shunyavkash.com>
2025-04-09 17:29:46 +0530

    Add Single Client Route

--- src/routes/client/clientRoutes/clientRoute.js
+++ src/routes/client/clientRoutes/clientRoute.js
@@ -4,12 +4,14 @@ import {
   getAllClients,
   updateClient,
   deleteClient,
+  getSingleClient,
 } from "../controller/clientController.js";
 
 const router = express.Router();
 
 router.post("/", createClient);
 router.get("/", getAllClients);
+router.get("/:id", getSingleClient);
 router.put("/:id", updateClient);
 router.delete("/:id", deleteClient);
 
--- src/routes/client/controller/clientController.js
+++ src/routes/client/controller/clientController.js
@@ -1,5 +1,6 @@
 import Client from "../../../models/Client.js";
 
+// Create Client
 export const createClient = async (req, res) => {
   try {
     const client = await Client.create(req.body);
@@ -9,11 +10,26 @@ export const createClient = async (req, res) => {
   }
 };
 
+// Get All Clients
 export const getAllClients = async (req, res) => {
   const clients = await Client.find();
   res.json(clients);
 };
 
+// Get Single Client by ID
+export const getSingleClient = async (req, res) => {
+  try {
+    const client = await Client.findById(req.params.id);
+    if (!client) {
+      return res.status(404).json({ message: "Client not found" });
+    }
+    res.json(client);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Update Client
 export const updateClient = async (req, res) => {
   const client = await Client.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -21,6 +37,7 @@ export const updateClient = async (req, res) => {
   res.json(client);
 };
 
+// Delete Client
 export const deleteClient = async (req, res) => {
   await Client.findByIdAndDelete(req.params.id);
   res.json({ message: "Client deleted" });
