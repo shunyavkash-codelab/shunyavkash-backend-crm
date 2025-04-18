90e0f3414aa5636ff73bc617d803115a3c89f777
shunyavkash-satish-kanaujiya <satish.kanaujiya@shunyavkash.com>
2025-04-18 10:53:59 +0530

    implement functionality for invoice PDF generation

--- src/routes/invoice/invoiceRoutes/invoiceRoute.js
+++ src/routes/invoice/invoiceRoutes/invoiceRoute.js
@@ -1,3 +1,30 @@
+// import express from "express";
+// import {
+//   createInvoice,
+//   deleteInvoice,
+//   getInvoiceById,
+//   getInvoices,
+//   updateInvoiceStatus,
+// } from "../../invoice/controller/invoiceController.js";
+// import protect from "../../../middlewares/authMiddleware.js";
+// import authorizeRoles from "../../../middlewares/roleMiddleware.js";
+
+// const router = express.Router();
+
+// // Admin only
+// router.post("/", protect, authorizeRoles("Admin"), createInvoice);
+// router.get("/", protect, authorizeRoles("Admin"), getInvoices);
+// router.get("/:id", protect, authorizeRoles("Admin"), getInvoiceById);
+// router.put(
+//   "/:id/status",
+//   protect,
+//   authorizeRoles("Admin"),
+//   updateInvoiceStatus
+// );
+// router.delete("./:id", protect, authorizeRoles("Admin"), deleteInvoice);
+
+// export default router;
+
 import express from "express";
 import {
   createInvoice,
@@ -5,22 +32,29 @@ import {
   getInvoiceById,
   getInvoices,
   updateInvoiceStatus,
+  regenerateInvoicePDF,
 } from "../../invoice/controller/invoiceController.js";
-import protect from "../../../middlewares/authMiddleware.js";
-import authorizeRoles from "../../../middlewares/roleMiddleware.js";
+// import protect from "../../../middlewares/authMiddleware.js";
+// import authorizeRoles from "../../../middlewares/roleMiddleware.js";
 
 const router = express.Router();
 
 // Admin only
-router.post("/", protect, authorizeRoles("Admin"), createInvoice);
-router.get("/", protect, authorizeRoles("Admin"), getInvoices);
-router.get("/:id", protect, authorizeRoles("Admin"), getInvoiceById);
+router.post("/", createInvoice);
+router.get("/", getInvoices);
+router.get("/:id", getInvoiceById);
 router.put(
   "/:id/status",
-  protect,
-  authorizeRoles("Admin"),
+
   updateInvoiceStatus
 );
-router.delete("./:id", protect, authorizeRoles("Admin"), deleteInvoice);
+router.delete("/:id", deleteInvoice);
+
+// Regenerate PDF on demand
+router.post(
+  "/:id/regenerate",
+
+  regenerateInvoicePDF
+);
 
 export default router;
