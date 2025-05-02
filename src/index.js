732dda4cca13b6c99e038f09c7d5360261dc4c62
Jaydip Suvagiya <jaydip.suvagiya@shunyavkash.com>
2025-05-02 14:26:23 +0530

    feat: add cors middleware

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import connectDB from './configs/db.js';
 import router from './router.js';
-import corsMiddleware from './middlewares/corsMiddleware.js';
+import cors from './middlewares/corsMiddleware.js';
 import './cronJobs/invoiceCleanup.js';
 import { PORT } from './configs/environmentConfig.js';
 import { requestTimingLogger } from './middlewares/requestTimingLoggerMiddleware.js';
 import logger from './utils/loggerUtils.js';
 
 const app = express();
-app.use(corsMiddleware);
+app.use(cors());
 app.use(requestTimingLogger);
 app.use(express.json());
 
