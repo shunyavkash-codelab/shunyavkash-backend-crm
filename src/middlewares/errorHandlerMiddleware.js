e71ae51900ab0951dc0a5ec055702150bef3ec98
Jaydip Suvagiya <jaydip.suvagiya@shunyavkash.com>
2025-05-02 14:35:03 +0530

    feat: error handling middleware add

---
+++ src/middlewares/errorHandlerMiddleware.js
@@ -0,0 +1,8 @@
+import logger from '../utils/loggerUtils.js';
+import SendResponse from '../utils/sendResponseUtils.js';
+
+// eslint-disable-next-line no-unused-vars
+export default function errorHandler(err, req, res, next) {
+  logger.error(err.stack);
+  return SendResponse(res, 500, false, 'Something went wrong!😬');
+}
